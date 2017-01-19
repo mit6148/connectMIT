008da9aac5e3b1ae9a06d35db6e3c17d99e3231a
Hannah Chen <hchen15@mit.edu>
2017-01-19 12:45:25 -0500

    start

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+
+
+/********************************************************************
+* App Setup 
+*********************************************************************/
+var app = express();
+
+app.use(bodyParser.json());
+// app.use(session({ secret : '6170', resave : true, saveUninitialized : true }));
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/public/html'));
+app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/public/html');
+var port = process.env.PORT || 3000;
+
+/********************************************************************
+* Database Setup
+*********************************************************************/
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rendezvousdb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback) {
+    console.log('database connected');
+});
+
+
+/**
+* Has app listen on correct port 
+*/
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
+
+module.exports = app;
