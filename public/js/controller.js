b79112225d62b36e229bdfa88a9ba8981f9d612d
Saron Teklezgi <sarontek@mit.edu>
2017-01-22 18:55:32 -0500

    course selector

--- public/js/controller.js
+++ public/js/controller.js
@@ -68,6 +68,85 @@ $(function(){
         });
 	});
 
+    //course major picker
+    $('#addAll').click(function() {
+        // move all values from #allCourses to #yourCourses
+        var allCourses = document.getElementById('allCourses').options;
+        var yourCourses = document.getElementById('yourCourses');
+        for (i = 0; i < allCourses.length; i++) {
+            var option = document.createElement('option');
+            option.value = allCourses[i].value;
+            option.innerHTML = allCourses[i].text;
+            yourCourses.appendChild(option);
+        }
+        $('#allCourses').find('option').remove();
 
+    });
+
+    $('#addSome').click(function() {
+        // move selected values from #allCourses to #yourCourses
+        var allCourses = document.getElementById('allCourses');
+        var yourCourses = document.getElementById('yourCourses');
+        for (i = 0; i < allCourses.options.length; i++) {
+            var option = allCourses.options[i];
+            if (option.selected) {
+                var newOption = document.createElement('option');
+                newOption.value = option.value;
+                newOption.innerHTML = option.text;
+                yourCourses.appendChild(newOption);
+            }
+        }
+        $('#allCourses :selected').remove();
+    });
+
+    $('#removeSome').click(function() {
+        // move selected values from #allCourses to #yourCourses
+        var allCourses = document.getElementById('allCourses');
+        var yourCourses = document.getElementById('yourCourses');
+        for (i = 0; i < yourCourses.options.length; i++) {
+            var option = yourCourses.options[i];
+            if (option.selected) {
+                var newOption = document.createElement('option');
+                newOption.value = option.value;
+                newOption.innerHTML = option.text;
+                allCourses.appendChild(newOption);
+            }
+        }
+        $('#yourCourses :selected').remove();
+    });
+
+    $('#removeAll').click(function() {
+        // move all values from #yourCourses to #allCourses
+        var allCourses = document.getElementById('allCourses');
+        var yourCourses = document.getElementById('yourCourses').options;
+        for (i = 0; i < yourCourses.length; i++) {
+            var option = document.createElement('option');
+            option.value = yourCourses[i].value;
+            option.innerHTML = yourCourses[i].text;
+            allCourses.appendChild(option);
+        }
+        $('#yourCourses').find('option').remove();
+    });
+
+    $('.moveCourse').click(function() {
+        var yourCourses = document.getElementById('yourCourses');
+        var concatCourses = ""
+        for (i = 0; i < yourCourses.options.length; i++) {
+            concatCourses += yourCourses.options[i].text + ',';
+        }
+        document.getElementById('selectedCourses').value = concatCourses.substring(0, concatCourses.length - 1);
+
+        var allCourses = $('#allCourses option')
+        allCourses.sort(function(a, b) {
+            return a.value - b.value;
+        })
+        $('#allCourses').html(allCourses);
+
+        var yourCourses = $('#yourCourses option')
+        yourCourses.sort(function(a, b) {
+            return a.value - b.value;
+        })
+        $('#yourCourses').html(yourCourses);
+    });
 
 });
