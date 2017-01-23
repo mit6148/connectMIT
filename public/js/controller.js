75d8a37f25b13a72534b14b07625fca60e6bce5a
Saron Teklezgi <sarontek@mit.edu>
2017-01-23 14:46:12 -0500

    reorganize

--- public/js/controller.js
+++ public/js/controller.js
@@ -68,17 +68,6 @@ $(function(){
         });
 	});
 
-    $('#connectBtn').on('click', function() {
-        var email = $('#connectBtn').attr('class').split(" ")[2]
-        $.ajax({
-            email: email,
-            type: 'PUT',
-            success: function(data) {
-                window.location.assign("/users/connect/" + email);
-            }
-        });
-    });
-
     //course major picker
     $('#addAll').click(function() {
         // move all values from #allCourses to #yourCourses
@@ -436,5 +425,16 @@ $(function(){
             }
         });
   });
+
+  $('#connectBtn').on('click', function() {
+    var email = $('#connectBtn').attr('class').split(" ")[2]
+    $.ajax({
+        email: email,
+        type: 'PUT',
+        success: function(data) {
+            window.location.assign("/users/connect/" + email);
+        }
+    });
+});
 
 });
