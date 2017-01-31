f4327675d57c6d59e67594b9e6cfb732f82081b7
Hannah Chen <hchen15@mit.edu>
2017-01-31 00:56:28 -0500

    cleaning up

--- utils/utils.js
+++ utils/utils.js
@@ -4,30 +4,6 @@ var utils = (function() {
 
   var _utils = {};
 
-  /**
-    Send an error code with success:false and error message
-    as provided in the arguments to the response argument provided.
-    The caller of this function should return after calling
-  */
-  // _utils.sendErrorResponse = function(res, errorCode, error) {
-  //   res.status(errorCode).json({
-  //     success: false,
-  //     err: error
-  //   }).end();
-  // };
-
-  /**
-    Send a 200 OK with success:true in the request body to the
-    response argument provided.
-    The caller of this function should return after calling
-  */
-  // _utils.sendSuccessResponse = function(res, content) {
-  //   res.status(200).json({
-  //     success: true,
-  //     content: content
-  //   }).end();
-  // };
-
   var smtpConfig = {
     host: 'smtp.gmail.com',
     port: 465,
